57a5071174522172b1008c1a460919a81037f3e6
Ahmad Abuyahya <ahmadabuyahya998@gmail.com>
2024-07-30 11:06:44 +0300

    initialize project

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import Form from "lead-gateway";
+
+function App() {
+  return (
+    <>
+      <div>
+        lead gateway consumer 1
+        <div>
+          <Form />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
